99bf7384f52135bdce60c82f6aaceb5a6a9deac5
De Lima Silva <geisla.de.lima.silva@accenture.com>
2018-02-16 00:05:12 -0200

    Trabalhando com componentes

---
+++ app1.2/src/app/topo/topo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-topo',
+    templateUrl: './topo.component.html',
+    // template inline: template: '<p>Esse é o componente topo</p>',
+    //styles: ['.exemplo { color: red }']
+    styleUrls: ['./topo.component.css']
+})
+export class TopoComponent{}
